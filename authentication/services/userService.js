300015633c3c5c1227969e3e00122ad2d0205e1a
Chandan Kumar Shaw <chandan.kumar.shaw@sap.com>
2024-11-05 02:25:11 +0530

    Included user crypto address in the get user info response

--- authentication/services/userService.js
+++ authentication/services/userService.js
@@ -1,6 +1,9 @@
 const User = require('../models/User');
+const walletService = require('./walletService');
+const { decryptMnemonic } = require('../utils/encrypt');
 
 exports.getUser = async (name) => {
-    const user = await User.findOne({ username: name }, '_id username email');
-    return user;
+    let user = await User.findOne({ username: name }, '_id username email mnemonic');
+    const wallet = await walletService.getWalletFromMnemonic(decryptMnemonic(user.mnemonic));
+    return { _id: user._id, username: user.username, email: user.email, address: wallet.address };
 }
